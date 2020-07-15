a9dd9d5840c0f8a4a36d2a071115f695b1981731
mbulelo20 <mbulelopani09@gmail.com>
2020-07-15 12:09:37 +0200

    Updates

---
+++ precache-manifest.1453243c9824ff71688c6492d02cb785.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f241699aada6a611f9d3c07744f3fd0a",
+    "url": "/visitour/index.html"
+  },
+  {
+    "revision": "9017578ea89ebd7e9ab3",
+    "url": "/visitour/static/css/2.e4346227.chunk.css"
+  },
+  {
+    "revision": "a7e304eff8d35c31e522",
+    "url": "/visitour/static/css/main.80cdfb27.chunk.css"
+  },
+  {
+    "revision": "9017578ea89ebd7e9ab3",
+    "url": "/visitour/static/js/2.1e370ee1.chunk.js"
+  },
+  {
+    "revision": "53678f98854006ad67866662f0d9277d",
+    "url": "/visitour/static/js/2.1e370ee1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a7e304eff8d35c31e522",
+    "url": "/visitour/static/js/main.ec695ec1.chunk.js"
+  },
+  {
+    "revision": "897a13187765dc394c90",
+    "url": "/visitour/static/js/runtime-main.4d17de55.js"
+  }
+]);
