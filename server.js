ebb0e60d30a2c5e6f52a70e3ddfbac312a77ced2
Thapelo Tsotetsi <me@tsotetsithapelo.co.za>
2020-08-05 21:27:51 +0200

    Fix error on the port variable.

--- server.js
+++ server.js
@@ -14,6 +14,6 @@ app.use('/api/users', require('./components/users'));
 app.use(express.static('client/build'));
 app.get('/', (req, res) => res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html')));
 
-const PORT = process.env.Port || 5000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`server started on port ${PORT}`));
