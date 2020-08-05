0d78f2794e2bd12d90610c8fe6c919beeda1e89f
Thapelo Tsotetsi <me@tsotetsithapelo.co.za>
2020-08-05 18:58:00 +0200

    Update minor issues

--- server.js
+++ server.js
@@ -1,23 +1,26 @@
 const express = require('express');
-const connectDB = require('./config/db')
-const path = require('path')
+const connectDB = require('./config/db');
+const path = require('path');
 const app = express();
 
 connectDB();
 
-app.use(express.json({extended:false}))
-
-app.get('/', (req, res) => res.json({msg: 'hi there...'}))
+app.use(express.json({extended:false}));
 
 app.use('/api/comments', require('./components/comments'));
 app.use('/api/auth', require('./components/auth'));
 app.use('/api/users', require('./components/users'));
 
 if(process.env.NODE_ENV === 'production'){
-    app.use(express.static('client/build'))
- 
-    app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html')))
+
+    app.use(express.static('client/build'));
+    app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html')));
+
+} else {
+
+    app.get('/', (req, res) => res.json({msg: 'hi there...'}));
+
 }
 const PORT = process.env.Port || 5000;
 
-app.listen(PORT, () => console.log(`server started on port ${PORT}`))
+app.listen(PORT, () => console.log(`server started on port ${PORT}`));
