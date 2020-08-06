ae2b3f5670ddb696637d88b0466b8b5c602194f6
mbulelo20 <mbulelopani09@gmail.com>
2020-08-06 09:27:26 +0200

    deploy

--- server.js
+++ server.js
@@ -14,10 +14,10 @@ app.use('/api/auth', require('./components/auth'));
 app.use('/api/users', require('./components/users'));
 
 if(process.env.NODE_ENV === 'production'){
-    app.use(express.static('client/build'))
- 
-    app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html')))
-}
+    app.use(express.static(path.join(__dirname, "client/build")));
+    app.get('/', (req, res) => {
+        res.sendFile(path.join(`${__dirname}/client/build/index.html`));
+});}
 const PORT = process.env.Port || 5000;
 
 app.listen(PORT, () => console.log(`server started on port ${PORT}`))
