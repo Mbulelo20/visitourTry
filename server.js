8475e28941160a02bf99f9bb0db8e4a8e7646171
mbulelo20 <mbulelopani09@gmail.com>
2020-08-06 14:19:35 +0200

    deploy

--- server.js
+++ server.js
@@ -18,6 +18,6 @@ if(process.env.NODE_ENV === 'production'){
     app.get('/', (req, res) => {
         res.sendFile(path.join(`${__dirname}/client/build/index.html`));
 });}
-const PORT = process.env.Port || 5000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`server started on port ${PORT}`))
