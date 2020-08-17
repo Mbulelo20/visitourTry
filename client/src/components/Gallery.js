3461eeac2ade4cdd54352b96e9da4cb5c86c9e56
mbulelo20 <mbulelopani09@gmail.com>
2020-08-17 10:27:38 +0200

    update

--- client/src/components/Gallery.js
+++ client/src/components/Gallery.js
@@ -5,22 +5,20 @@ import CommentForm from './comments/CommentForm'
 
 const Gallery = () => {
     return (
-        <Fragment>
-            <div style={{backgroundColor:"white", width:'110%'}}>
-                <div class="card bg-primary">
-                    <h2 style={{fontFamily:'sans', fontWeight:'bolder'}}><center>Gallery</center></h2>
-                    <h5><center>Take a look on what's on offer, and explore!</center></h5>
+        <Fragment style={{backgroundColor:"white", width:'110%'}}>
+            <div class="card bg-primary">
+                <h2 style={{fontFamily:'sans', fontWeight:'bolder'}}><center>Gallery</center></h2>
+                <h5><center>Take a look on what's on offer, and explore!</center></h5>
+            </div>
+            <div class="card-deck" style={{paddingTop:'5em'}}>
+                <div class="card" >
+                    <div style={{width:'100%', paddingLeft:'-1em'}}>
+                        <MyCities/>
+                    </div>
                 </div>
-                <div class="card-deck" style={{paddingTop:'5em'}}>
-                    <div class="card" >
-                        <div style={{width:'100%', paddingLeft:'-1em'}}>
-                            <MyCities/>
-                        </div>
-                    </div>
-                    <div style={{width:'30%', height:"50%", backgroundColor:'white'}}>
-                        <CommentForm/>
-                        <Comments/>
-                    </div>
+                <div style={{width:'30%', height:"50%", backgroundColor:'white'}}>
+                    <CommentForm/>
+                    <Comments/>
                 </div>
             </div>
         </Fragment>
