ae83f8d2eb77e34693eed618c850da2d7789137d
mbulelo20 <mbulelopani09@gmail.com>
2020-08-17 10:16:58 +0200

    update

--- client/src/components/Gallery.js
+++ client/src/components/Gallery.js
@@ -6,7 +6,7 @@ import CommentForm from './comments/CommentForm'
 const Gallery = () => {
     return (
         <Fragment>
-            <div style={{backgroundColor:"white"}}>
+            <div style={{backgroundColor:"white", width:'110%'}}>
                 <div class="card bg-primary">
                     <h2 style={{fontFamily:'sans', fontWeight:'bolder'}}><center>Gallery</center></h2>
                     <h5><center>Take a look on what's on offer, and explore!</center></h5>
