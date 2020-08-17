1d282757d068f09fd0b2c771f3bb87c25f5c0e4d
mbulelo20 <mbulelopani09@gmail.com>
2020-08-17 09:04:21 +0200

    update

--- client/src/components/EachCity.js
+++ client/src/components/EachCity.js
@@ -6,14 +6,19 @@ const EachCity = ({city: {name,tag, link, pic}}) => {
     return (
         <Fragment>
         <div className="card " style={{width:'100%', margin:'0.8em', backgroundColor:"gray"}}>
-            <div>
-                <img src={pic} alt="city" style={{width:'50%', height:'85%', margin:'-1em'}} />
-            </div>
-            <div className="card-body" style={{paddingTop:"5em"}}>
-                <h2 class="card-title">{name}</h2>
-                <h5>{tag}</h5>
-                <Link to={link} class="btn btn-primary">More</Link>
-            </div>
+            <div className="row" >
+                <div class="col-sm">
+                    <img src={pic} alt="city" style={{width:'50%', height:'85%', margin:'-1em'}} />
+                </div>
+                <div class="col-sm">
+                    <div className="card-body" style={{paddingTop:"5em"}}>
+                        <h2 class="card-title">{name}</h2>
+                        <h5>{tag}</h5>
+                        <Link to={link} class="btn btn-primary">More</Link>
+                    </div>
+                </div>
+            </div >
+            
         </div>
     </Fragment>
     )
