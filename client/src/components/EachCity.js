56d748e49bfc9623c8bbbfe623cb72ab021b3b77
mbulelo20 <mbulelopani09@gmail.com>
2020-08-17 10:07:32 +0200

    update

--- client/src/components/EachCity.js
+++ client/src/components/EachCity.js
@@ -5,13 +5,13 @@ const EachCity = ({city: {name,tag, link, pic}}) => {
      
     return (
         <Fragment>
-        <div className="card " style={{width:'97%', backgroundColor:"silver", marginLeft:'0.5em'}}>
+        <div className="card " style={{width:'98.5%', backgroundColor:"silver", marginLeft:'0.5em', marginBottom:'1em'}}>
             <div className="row" style={{width:'100%'}} >
                 <div class="col-sm">
                     <img src={pic} alt="city" style={{width:'100%', height:'100%', margin:'1em'}} />
                 </div>
                 <div class="col-sm">
-                    <div className="card-body" style={{paddingTop:"2em"}}>
+                    <div className="card-body" style={{paddingTop:"3em"}}>
                         <h2 class="card-title">{name}</h2>
                         <h5>{tag}</h5>
                         <Link to={link} class="btn btn-primary">More</Link>
--- client/src/components/Gallery.js
+++ client/src/components/Gallery.js
@@ -6,19 +6,21 @@ import CommentForm from './comments/CommentForm'
 const Gallery = () => {
     return (
         <Fragment>
-            <div class="card bg-primary">
-                <h2 style={{fontFamily:'sans', fontWeight:'bolder'}}><center>Gallery</center></h2>
-                <h5><center>Take a look on what's on offer, and explore!</center></h5>
-            </div>
-            <div class="card-deck" style={{paddingTop:'5em'}}>
-                <div class="card" >
-                    <div style={{width:'100%', paddingLeft:'-1em'}}>
-                        <MyCities/>
-                    </div>
+            <div style={{backgroundColor:"white"}}>
+                <div class="card bg-primary">
+                    <h2 style={{fontFamily:'sans', fontWeight:'bolder'}}><center>Gallery</center></h2>
+                    <h5><center>Take a look on what's on offer, and explore!</center></h5>
                 </div>
-                <div style={{width:'30%', height:"50%", backgroundColor:'white'}}>
-                    <CommentForm/>
-                    <Comments/>
+                <div class="card-deck" style={{paddingTop:'5em'}}>
+                    <div class="card" >
+                        <div style={{width:'100%', paddingLeft:'-1em'}}>
+                            <MyCities/>
+                        </div>
+                    </div>
+                    <div style={{width:'30%', height:"50%", backgroundColor:'white'}}>
+                        <CommentForm/>
+                        <Comments/>
+                    </div>
                 </div>
             </div>
         </Fragment>
