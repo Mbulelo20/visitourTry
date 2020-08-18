9fc8e5dba61bd2c1331eeae68af31429f0ee0e66
mbulelo20 <mbulelopani09@gmail.com>
2020-08-18 11:22:33 +0200

    alerts

--- client/src/context/alert/AlertState.js
+++ client/src/context/alert/AlertState.js
@@ -12,7 +12,7 @@ const AlertState = props => {
         
     const [state, dispatch] = useReducer(alertReducer, initialState);
     
-    const setAlert = (msg, type, timeout = 5000) => {
+    const setAlert = (msg, type, timeout = 3000) => {
         const id = uuidv4();
         dispatch({
             type: SET_ALERT,
